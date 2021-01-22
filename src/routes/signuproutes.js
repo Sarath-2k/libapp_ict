f8615c8b6fd84957a9ab98ba32257ae76f11a2bb
I S Sarath <sarath2000is@gmail.com>
2021-01-23 02:23:25 +0530

    submission

---
+++ src/routes/signuproutes.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const signupRouter = express.Router();
+signupRouter.get('/', function(req, res) {
+    res.render("signup");
+})
+module.exports = signupRouter;
